50384781b9508398a37296293c9d08a26a7037a0
Bastien Lutz <bastien.lutz@in2code.de>
2025-05-04 16:25:05 +0200

    [FEATURE] Add TCA for api base configuration
    Related: #3

--- packages/surfcamp-base/Configuration/TCA/tx_surfcampbase_api_base.php
+++ packages/surfcamp-base/Configuration/TCA/tx_surfcampbase_api_base.php
@@ -1,10 +1,62 @@
 <?php
 
+$ll = 'LLL:EXT:surfcamp_base/Resources/Private/Language/locallang_be.xlf:tx_surfcampbase_api_base';
+
 return [
     'ctrl' => [
-
+        'title' => $ll,
+        'label' => 'name',
+        'iconfile' => 'EXT:surfcamp_base/Resources/Public/Icons/configuration.svg',
+        'enablecolumns' => [
+            'disabled' => 'hidden',
+            'starttime' => 'starttime',
+            'endtime' => 'endtime',
+        ],
+    ],
+    'types' => [
+        0 => [
+            'showitem' => '
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                name,base_url,additional_headers,endpoints,
+            --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                hidden,--palette--;;timeRestriction,',
+        ],
     ],
     'columns' => [
-
+        'name' => [
+            'label' => $ll . '.name',
+            'config' => [
+                'type' => 'input',
+                'eval' => 'trim',
+                'size' => 200,
+            ],
+        ],
+        'base_url' => [
+            'label' => $ll . '.base_url',
+            'config' => [
+                'type' => 'link',
+                'required' => true,
+                'allowedTypes' => ['url'],
+            ],
+        ],
+        'additional_headers' => [
+            'label' => $ll . '.additional_headers',
+            'description' => $ll . '.additional_headers.description',
+            'config' => [
+                'type' => 'json',
+                'required' => true,
+            ],
+        ],
+        'endpoints' => [
+            'label' => $ll . '.endpoints',
+            'config' => [
+                'type' => 'inline',
+                'foreign_table' => 'tx_surfcampbase_api_endpoint',
+                'foreign_field' => 'base',
+            ],
+        ],
+    ],
+    'palettes' => [
+        'timeRestriction' => ['showitem' => 'starttime, endtime'],
     ],
 ];
