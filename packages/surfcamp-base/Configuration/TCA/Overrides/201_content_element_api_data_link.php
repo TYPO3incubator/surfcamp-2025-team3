3aefb16b57f5fa634014a089ec02449b373c4736
Ali <ali-oezdemir_96@outlook.de>
2025-05-04 15:36:17 +0200

    first impl of api integration ce

---
+++ packages/surfcamp-base/Configuration/TCA/Overrides/201_content_element_api_data_link.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+defined('TYPO3') or die();
+
+$key = 'api_data_link';
+
+// Adds the content element to the "Type" dropdown
+ExtensionManagementUtility::addTcaSelectItem(
+    'tt_content',
+    'CType',
+    [
+        'label' => 'LLL:EXT:surfcamp-base/Resources/Private/Language/locallang_be.xlf:content_element.api_data_link',
+        'value' => $key,
+        'group' => 'default',
+    ],
+    'textmedia',
+    'after',
+);
+
+// Configure the default backend fields for the content element
+$GLOBALS['TCA']['tt_content']['types'][$key] = [
+    'showitem' => '
+            --palette--;;headers,
+            bodytext,
+        ',
+];
