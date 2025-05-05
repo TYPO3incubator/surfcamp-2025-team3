9dd5c91d0fd5dbfb50c2734aef482a746c70b461
Felix <321faily@gmail.com>
2025-05-05 19:33:32 +0200

    add resolving for uploaded template

---
+++ packages/surfcamp-base/Classes/UserFunctions/FileResolver.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TYPO3Incubator\SurfcampBase\UserFunctions;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
+use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Resource\Exception\ResourceDoesNotExistException;
+use TYPO3\CMS\Core\Resource\FileRepository;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+#[Autoconfigure(public: true)]
+readonly class FileResolver
+{
+    public function __construct(private FileRepository $fileRepository)
+    {
+    }
+
+    public function resolveContentsForFileReferenceLocalId(int|string $id): string
+    {
+        try {
+            $reference = $this->fileRepository->findByRelation('tt_content', 'template', $id);
+            if ($reference !== []) {
+                return $reference[0]->getOriginalFile()->getContents();
+            }
+            return '';
+        } catch (ResourceDoesNotExistException) {
+            return '';
+        }
+    }
+}
