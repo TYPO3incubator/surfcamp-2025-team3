b6ee39338ec4adfd92d365aad8a1a806787a2ded
Bastien Lutz <bastien.lutz@in2code.de>
2025-05-06 19:04:30 +0200

    [TASK] Override codeEditor value
    Related: #15

--- packages/surfcamp-base/Classes/Backend/Form/Element/ApiResponseFieldElement.php
+++ packages/surfcamp-base/Classes/Backend/Form/Element/ApiResponseFieldElement.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 namespace TYPO3Incubator\SurfcampBase\Backend\Form\Element;
 
-use TYPO3\CMS\Backend\Form\Element\AbstractFormElement;
+use TYPO3\CMS\Backend\Form\Element\CodeEditorElement;
 
-class ApiResponseFieldElement extends AbstractFormElement
+class ApiResponseFieldElement extends CodeEditorElement
 {
     public function render(): array
     {
-        $resultArray = [];
-        $html = [];
-        $resultArray['html'] = implode(LF, $html);
-        return $resultArray;
+        $this->data['parameterArray']['itemFormElValue'] = 'computed value';
+        return parent::render();
     }
 }
